6eee7f4a47f5ababf98c58bfa2e5a42f5e8eac27
Yasmine <91900711+yasmine-thandi@users.noreply.github.com>
2022-08-20 16:52:46 -0400

    Add files via upload

---
+++ project_clientside.js
@@ -0,0 +1,27 @@
+//The placement of the information regarding gender equity and inclusiveness
+ function box1() {
+
+     //Description
+     document.getElementById("problem").innerHTML = "<p> <h1> Who Is Involved In Gender Equity and Inclusiveness? </h1> Everyone can be affected by gender inequality due to gender stereotypes and gender roles that appear throughout peoples' lifetimes. This is why we need gender equity and gender equality! Unfortunately, there are so many different kinds of problematic situations people face due to gender inequality, it is difficult and wrong to pinpoint specific situations. As for gender equity and inclusiveness, everyone can be involved! In fact, real change regarding gender equity and inclusiveness will not be completely obtained unless everyone does their part. There will be a list of ways one can promote gender equity and inclusiveness on one of the buttons below. </p>";
+
+}function box2() {
+
+     //Description
+     document.getElementById("problem").innerHTML = "<p> <h1> What Is Gender Equity? </h1> Gender equity is the process of creating and ensuring the fair treatment amongst all genders. It ensures that there is equality amongst all people regardless of the social and historical disadvantges that were experienced by some. Keep in mind, gender equity does not mean that the different genders need to become the same, but rather that the rights, reponsibilities and chances that an individual receives are not dependant on one's gender. So feel free to be yourself! <h2> What Is Inclusion? </h2> The definition of inclusion is in it's name! Inclusion is the act of including. The concept of inclusion consists of including everyone/not leaving anyone out despite their differences or history of being excluded (whether that be because of gender, sexuality, race, culture, educational background, personality, etc.). Individuals practice inclusion when they recognize and accept peoples' differences. Everyone should feel important and valued! </p>";
+
+}function box3() {
+
+     //Description
+     document.getElementById("problem").innerHTML = "<p> <h1> When and Where Does Gender Equity and Inclusiveness Occur? </h1> While no one can really pinpoint exactly when efforts for gender equity occurred (as even the smallest steps of individuals count), gender equity has been legalized as a human right from 1948 till present date. Many movements, such as the Suffrage Movement, were created to achieve gender equity and inclusiveness. Gender equality (the goal of gender equity) became a part of the international human rights law, where it stated 'All human beings are born free and equal in dignity and rights' on December 10th, 1948. As for where gender equity and inclusiveness occur, well, it occurs globally. Gender equity and inclusiveness must occur globally and all the time because gender inequality has been a global issue for years. In fact, gender equity and inclusiveness have evolved gender inequality over the decades, but has not entirely fixed it. </p>";
+
+}function box4() {
+
+     //Description
+     document.getElementById("problem").innerHTML = "<p> <h1> Why Create and Promote Gender Equity and Inclusiveness? </h1> First off, promoting gender equity and inclusiveness is obviously important as it resolves the issue of gender inequality in society. No one should be at a disadvantage because of their differences and gender. Other noteworthy reasons why one should create and promote gender equity and inclusiveness is because there will be a diverse representation of people in politics (hence, political decisions made based on inclusivity), increased economic opportunities for everyone, fewer labor shortages, more creativity and innovation in tech, less wage gaps and overall, more happier people. <h2> How Do We Create and Promote Gender Equity and Inclusiveness? </h2> Some ways an individual can create and promote gender equity and inclusiveness in their own households is by distributing household chores and childcare equally, supporting people that are disadavantaged due to gender inequality, rejecting chauvinist and racist attitudes, listening and reflecting in life, requesting the same salary for equal work and including everyone. An individual can also get educated or educate others on gender equity and inclusiveness (like this webpage is doing! yay!), share gender equality resources, vote for inclusive leaders, get involved through organizations and projects that promote gender equity, and lastly, accept the fact that gender inequality exists, and that gender equity and inclusivity need to prevail. These are only a few ways to take part in gender equity and inclusiveness, one can always research on how to do more. </p>";
+
+}
+
+function outputComments() {
+//Displaying comments to user
+     document.getElementById("#comments").value;
+}
---
+++ project_serverside.js
@@ -0,0 +1,26 @@
+//Saving the comments
+$(document).ready(function(){ 
+  
+  $(".primaryContained").on('click', function(){
+  $(".comment").addClass("commentClicked");
+});
+$("textarea").on('keyup.enter', function(){
+  $(".comment").addClass("commentClicked");
+});
+});
+
+new Vue({
+  el: "#app",
+  data:{
+     title: 'Add a comment',
+    newItem: '',
+    item: [],
+  },
+  methods:{
+    addItem  (){
+    this.item.push(this.newItem);
+      this.newItem = "";
+    }
+}
+
+});
